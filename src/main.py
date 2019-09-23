1177f03019afed41ad23bf86350bd679e4553d97
Claire Feng <yyf2121@gmail.com>
2019-09-23 22:43:02 +1200

    Add class

--- src/main.py
+++ src/main.py
@@ -3,3 +3,28 @@ from test import fn, num
 fn()
 
 print("Hello World", num)
+
+
+class phoneFactory:
+    def __init__(self, num):
+        self.nEmployees = num
+
+    def hire(self, nNewEmployees):
+        self.nEmployees += nNewEmployees
+        self.manager = 'happy'
+        return self
+
+    @staticmethod
+    def tenEmployees():
+        return phoneFactory(10).hire(5)
+
+    def printNumEmployees(self):
+        print(self.nEmployees)
+
+
+phoneFactory1 = phoneFactory(5)
+phoneFactory1.printNumEmployees()
+phoneFactory1.hire(10).hire(20).printNumEmployees()
+
+phoneFactory2 = phoneFactory(3)
+# phoneFactory.tenEmployees().hire(6).printNumEmployees()
